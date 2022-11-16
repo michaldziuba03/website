08072da646447f1ddbbc4de1b2baea1e1bac7770
mich4ld <mchldziuba@gmail.com>
2022-11-16 02:10:30 +0100

    add self-hosted google fonts

---
+++ gatsby-browser.tsx
@@ -0,0 +1,3 @@
+import './src/global.scss';
+import '@fontsource/prompt/500.css'
+import '@fontsource/prompt/700.css'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import type { HeadFC, PageProps } from "gatsby"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <div>
+    <h1>
       Hello World
-    </div>
+    </h1>
   )
 }
 
