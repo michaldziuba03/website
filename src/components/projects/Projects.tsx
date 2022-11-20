ea9debce1338f7452ac5d77c7952313d8b4ed1b1
mich4ld <mchldziuba@gmail.com>
2022-11-20 19:49:30 +0100

    init project section

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Project } from './Project';
 import './Projects.scss';
 
 export const Projects = () => {
     return (
         <div className='projects'>
+            <h1 className='projects__headline'>Projects</h1>
 
+            <Project />
+            <Project />
         </div>
     )
 }
