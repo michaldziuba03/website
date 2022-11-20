7c1f67e9e067b9a6e7083def8709c69621ed557b
mich4ld <mchldziuba@gmail.com>
2022-11-20 03:07:34 +0100

    implement custom scrollbar

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,7 +6,11 @@ import './About.scss';
 export const About = () => {
     return (
         <div className='about'>
-            <StaticImage className='about__me' alt='me' src='../../images/cooler-michal.jpg' />
+            <StaticImage 
+                className='about__me' 
+                alt='me' 
+                src='../../images/cooler-michal.jpg'
+            />
             <div className='about__details'>
                 <h1 className='about__headline'>About</h1>
                 <p className='about__description'>My name is Michał and I’m 19 years old from Poland. I started with programming in 2018. I’m intested in web development, operating systems (like Linux) and open-source-software movement. Besides programming I also like video games, history and dogs.</p>
