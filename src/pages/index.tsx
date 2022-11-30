684bfeb86de233fc966fc8bfe480ccdbcedcd2ec
mich4ld <mchldziuba@gmail.com>
2022-11-30 17:57:55 +0100

    create contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Hero } from "../sections/hero/Hero"
 import { Header } from "../components/header/Header"
 import { Footer } from "../components/footer/Footer"
 import { About } from "../sections/about/About"
+import { Contact } from "../sections/contact/Contact"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
@@ -12,6 +13,7 @@ const IndexPage: React.FC<PageProps> = () => {
       <main>
         <Hero />
         <About />
+        <Contact />
       </main>
       <Footer />
     </>
