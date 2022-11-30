b66274a0f80acf7ec8748c72700244dbf55b377f
mich4ld <mchldziuba@gmail.com>
2022-11-30 07:48:48 +0100

    make typography more consistent

---
+++ src/components/typography/index.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import React, { FC, PropsWithChildren } from 'react';
+import './Typography.scss';
+
+interface CustomTypography extends PropsWithChildren {
+    className?: string;
+}
+
+export const SectionH1: FC<CustomTypography> = (props) => {
+    const className = classNames('section-h1', props.className);
+
+    return (
+        <h1 className={className}>
+            { props.children }
+        </h1>
+    )
+}
+
+export const Description: FC<CustomTypography> = (props) => {
+    const className = classNames('paragraph', props.className);
+
+    return (
+        <p className={className}>
+            { props.children }
+        </p>
+    )
+}
