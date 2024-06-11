b17d7ef1b2aa8aabd53f85f1174f327dca34894e
Michał Dziuba <mail@michaldziuba.dev>
2024-06-12 01:57:48 +0200

    feat(blog): add read time

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import { remarkReadingTime } from './remark-reading-time.mjs';
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind(), react(), icon()]
 });
