b6e808e35d264d7d781bcce85e34868d1381014e
Michał Dziuba <mail@michaldziuba.dev>
2024-06-12 01:47:05 +0200

    feat(blog): add pagination

--- src/content/blog/markdown-style-guide.md
+++ src/content/blog/markdown-style-guide.md
@@ -1,7 +1,7 @@
 ---
 title: 'Building An E-Commerce Store With NextJS'
 description: 'This is a guide on how to build a newsletter application that allows users to subscribe to a mailing list using a Google or GitHub account. It uses Next.js, Firebase, and Novu. Part 2.'
-pubDate: 'Jul 01 2022'
+pubDate: 'Jul 01 2024'
 heroImage: '/blog-placeholder-1.jpg'
 ---
 
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const blogSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	// Transform string to Date object
+	pubDate: z.coerce.date(),
+	updatedDate: z.coerce.date().optional(),
+	heroImage: z.string().optional(),
 });
 
+const blog = defineCollection({
+	type: 'content',
+	schema: blogSchema,
+});
+
+export type BlogPost = z.infer<typeof blogSchema>;
 export const collections = { blog };
