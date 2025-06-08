b1930c679dea27bd58f5d15e0ee2e5f575a759cb
Michał Dziuba <mail@michaldziuba.dev>
2025-06-09 00:58:51 +0200

    refactor: init single blog post view

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   const now = new Date();
 
   return (
-    <div className="p-8 container flex w-full flex-col justify-between gap-4 sm:flex-row text-slate-700 border-t border-gray-300">
+    <footer className="p-8 container flex w-full flex-col justify-between gap-4 sm:flex-row text-slate-700 border-t border-gray-300">
       <span className='text-sm'>© {now.getFullYear()} <span className='font-semibold'>Michał Dziuba</span>. All rights reserved.</span>
 
       <div className='flex gap-6 text-xl'>
@@ -13,6 +13,6 @@ export function Footer() {
         <SiX />
         <SiInstagram />
       </div>
-    </div>
+    </footer>
   );
 }
